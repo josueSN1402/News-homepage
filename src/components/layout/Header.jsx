28619715f16f2632e079e27c00c80b7de2676888
Josue Sanchez Nima <josue.sanchez.n1402@hotmail.com>
2022-11-02 22:58:29 -0500

    Animation to open menu on mobile

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,10 +1,9 @@
+import { useState, useEffect } from 'react';
 import HeaderStyles from '../../scss/layout/Header.module.scss';
 import Logo from '../../assets/images/logo.svg';
 import IconMenu from '../../assets/images/icon-menu.svg';
-
-const openMenu = () => {
-	
-}
+import MobileMenuStyles from '../../scss/components/MobileMenu.module.scss';
+import IconClose from '../../assets/images/icon-menu-close.svg';
 
 const NavbarItem = ({children}) => {
 	return (
@@ -17,28 +16,48 @@ const NavbarItem = ({children}) => {
 }
 
 function Header() {
+	const [open, setOpen] = useState(false);
+
+	function MobileMenu(status) {
+		return (
+			<div className={`${MobileMenuStyles.MobileMenuContainer}${open ? ` ${MobileMenuStyles.Disable}` : ''}`}>
+				<div className={MobileMenuStyles.MobileMenu}>
+					<img
+						src={IconClose}
+						alt='Close'
+						className={MobileMenuStyles.IconClose}
+						onClick={()=> setOpen(!open)}
+					/>
+				</div>
+			</div>
+		);
+	}
+
 	return (
-		<header className={HeaderStyles.NewHeader}>
-			<a href='/'>
-				<img src={Logo} alt="W." className={HeaderStyles.Logo} />
-			</a>
-			<nav className={HeaderStyles.NavbarContianer}>
-				<ul className={HeaderStyles.Navbar}>
-					<NavbarItem>Home</NavbarItem>
-					<NavbarItem>New</NavbarItem>
-					<NavbarItem>Popular</NavbarItem>
-					<NavbarItem>Trending</NavbarItem>
-					<NavbarItem>Categories</NavbarItem>
-				</ul>
-				<img
-					src={IconMenu}
-					className={HeaderStyles.IconMenu}
-					aria-label='Open Menu'
-					alt="Icon menu"
-					onClick={openMenu}
-				/>
-			</nav>
-		</header>
+		<>
+			<header className={HeaderStyles.NewHeader}>
+				<a href='/'>
+					<img src={Logo} alt="W." className={HeaderStyles.Logo} />
+				</a>
+				<nav className={HeaderStyles.NavbarContianer}>
+					<ul className={HeaderStyles.Navbar}>
+						<NavbarItem>Home</NavbarItem>
+						<NavbarItem>New</NavbarItem>
+						<NavbarItem>Popular</NavbarItem>
+						<NavbarItem>Trending</NavbarItem>
+						<NavbarItem>Categories</NavbarItem>
+					</ul>
+					<img
+						src={IconMenu}
+						className={HeaderStyles.IconMenu}
+						aria-label='Open Menu'
+						alt="Icon menu"
+						onClick={()=> setOpen(!open)}
+					/>
+				</nav>
+			</header>
+			{open && <MobileMenu status={open} />}
+		</>
 	);
 }
 
